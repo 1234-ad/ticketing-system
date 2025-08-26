c2b92cbcdfee1c3f769e3e0a2b2eb96847e7e499
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-08-26 10:33:37 +0530

    Add CommentRepository interface

---
+++ backend/src/main/java/com/ticketing/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.ticketing.repository;
+
+import com.ticketing.model.Comment;
+import com.ticketing.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByTicketOrderByCreatedAtAsc(Ticket ticket);
+    List<Comment> findByTicketIdOrderByCreatedAtAsc(Long ticketId);
+}
